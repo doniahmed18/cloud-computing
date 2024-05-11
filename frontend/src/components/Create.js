fe81cd7ec253431b21a5eeb4e83ce49e7b34afc3
Hussein Gamal <105026648+Huss8in@users.noreply.github.com>
2024-05-12 00:24:43 +0300

    Dashboard Done

---
+++ frontend/src/components/Create.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Create.css';
+
+const Create = () => {
+  return (
+    <div className="create-page">
+      <h1>Create Page</h1>
+      <p>This is the create page content.</p>
+
+    </div>
+  );
+}
+
+export default Create;
---
+++ frontend/src/components/Update.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Update.css';
+
+const Update = () => {
+  return (
+    <div className="Update-page">
+      <h1>Update Page</h1>
+      <p>This is the Update page content.</p>
+
+    </div>
+  );
+}
+
+export default Update;

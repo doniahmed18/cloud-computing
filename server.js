dd0a21403fb4b7208b2585a030fc5659aa5e21a2
doniahmed18 <seifibr753@gmail.com>
2024-05-17 14:23:01 +0300

    before deploy

--- server.js
+++ server.js
@@ -1,39 +1,22 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
-
 const publicapiRoutes = require("./api/index")
+const path = require('path');
 const itemsRouter = require("./api/items") // Import the items router
-
 const port = 4000
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-// Mount the router
 app.use("/api", publicapiRoutes)
-// app.post("/create-item", async (req, res) => {
-//   const { itemName, imageUrl } = req.body
-//   const params = {
-//     TableName: "your-table-name",
-//     Item: {
-//       itemId: `${Date.now()}`,
-//       itemName,
-//       imageUrl,
-//     },
-//   }
-
-//   try {
-//     await dynamodb.put(params).promise()
-//     res.json({ message: "Item created successfully!" })
-//   } catch (err) {
-//     console.error("Error creating item in DynamoDB", err)
-//     res.status(500).json({ error: "Internal server error" })
-//   }
-// })
 app.use("/api", itemsRouter)
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+
+
